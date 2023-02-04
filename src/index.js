9d3deb5104ac09ab94eeb5c5dbad668d34a40abd
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-02-04 19:21:34 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = new Array();
+  let length = str.length;
+  let one = length % 2;
+
+  if (one == 1) return false;
+  else {
+    for (let i = 0; i <= str.length - 1; i++) {
+      switch (str[i]) {
+        case "{":
+          arr.push("}");
+          break;
+        case "(":
+          arr.push(")");
+          break;
+        case "[":
+          arr.push("]");
+          break;
+        case "1":
+          arr.push("2");
+          break;
+        case "3":
+          arr.push("4");
+          break;
+        case "5":
+          arr.push("6");
+          break;
+        case "|":
+          if (arr[arr.length - 1] == "|") {
+            arr.pop();
+            break;
+          }
+          arr.push("|");
+          break;
+        case "7":
+          if (arr[arr.length - 1] == "7") {
+            arr.pop();
+            break;
+          }
+          arr.push("7");
+          break;
+        case "8":
+          if (arr[arr.length - 1] == "8") {
+            arr.pop();
+            break;
+          }
+          arr.push("8");
+          break;
+        default:
+          if (arr.length == 0 || str[i] != arr[arr.length - 1]) {
+            return false;
+          }
+          arr.pop();
+      }
+    }
+    if (arr.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
